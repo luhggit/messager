bccebc771de11fe4151283a87e590ec92016230f
luhggit <342521162@qq.com>
2017-11-04 11:07:38 +0800

    activemq demo

---
+++ src/main/java/com/luhg/jms/queue/AppProducer.java
@@ -0,0 +1,11 @@
+package com.luhg.jms.queue;
+
+/**
+ * Created by luhg on 17-11-4.
+ */
+public class AppProducer {
+    private static final String url = "tcp://localhost:61616";
+    public static void main(String[] args) {
+
+    }
+}
